ce259be8e812ec0b678d4328a89ba1b82b2ade3d
pulkit786 <pulkit.mathur786@gmail.com>
2022-03-03 12:49:28 +0530

    Added conflict comment in git

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // https://www.npmjs.com/package/express
 const express = require('express');
 //added comment to test branching
+//added conflict comment
 // converts content in the request into parameter req.body
 // https://www.npmjs.com/package/body-parser
 const bodyParser = require('body-parser');
